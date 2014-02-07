e0062ac480e2e8dfcee9bac2faa1be8f1437c81f
sranso <sranso@gmail.com>
2014-02-07 17:12:56 -0500

    route to external data, add links

--- angular/activity-intermediate/app/js/controllers.js
+++ angular/activity-intermediate/app/js/controllers.js
@@ -2,25 +2,9 @@
 
 var animalTime = angular.module('animalTime', []);
 
-animalTime.controller('AnimalListCtrl', function($scope){
-  $scope.animals = [
-    {
-      "id": 1,
-      "name": "giraffe",
-      "hope": "to find someone tall enough to date",
-      "fear": "not being able to reach the leaves at the top of the tree"
-    },
-    {
-      "id": 2,
-      "name": "hippo",
-      "hope": "to one day be a ballerina",
-      "fear": "losing his keys"
-    },
-    {
-      "id": 3,
-      "name": "lion",
-      "hope": "that all the hyenas respect him",
-      "fear": "that he'll never remember who he was born to be"
-    }
-  ]
+animalTime.controller('AnimalListCtrl', function($scope, $http){
+  $http.get('animals/animals.json').success(function(data) {
+    $scope.animals = data;
+  })
+  
 });
